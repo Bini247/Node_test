7a35a9d2e5f19d084f984908fe5cdb929897b5ea
GABRIEL MOREIRA BINI <gabriel.bini@catolicasc.edu.br>
2019-10-08 20:15:32 -0300

    Form testimonials

--- routes/admin/posts.js
+++ routes/admin/posts.js
@@ -17,4 +17,21 @@ router.get('/create', function(req, res, next) {
     res.render('admin/posts/create');
 });
 
+router.post('/create', function(req, res, next) {
+    var testimonials = testimonialService.getTestimonialsLength();
+
+    var newId = testimonials.length + 1;
+    
+    var newPost = {};
+    newPost.title = req.body.title;
+    newPost.id = newId;
+    newPost.testimony = req.body.testimonial;
+    newPost.company = req.body.company;
+    newPost.name = req.body.name;
+
+    testimonialService.saveTestimonial(newPost);
+
+    res.redirect('/admin/posts');
+});
+
 module.exports = router;
--- services/testimonialsService.js
+++ services/testimonialsService.js
@@ -10,6 +10,25 @@ var getTestimonials = function(){
     return testimonials;
 }
 
+var saveFileTestimonial = function(testimonials){
+    var data = JSON.stringify(testimonials);
+    fs.writeFileSync(postsFilePath, data, 'utf8');
+}
+
+var getTestimonialsLength = function(){
+    var testimonials = getTestimonials();
+    return testimonials;
+}
+
+var saveTestimonial = function(newPost){
+
+    var testimonials = getTestimonials();
+    testimonials.push(newPost);    
+    saveFileTestimonial(testimonials);
+}
+
 module.exports = {
-    getTestimonials: getTestimonials
+    getTestimonials: getTestimonials,
+    saveTestimonial: saveTestimonial,
+    getTestimonialsLength: getTestimonialsLength
 }
